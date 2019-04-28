55fbfdf398f88152bf30899b9fbbb4c82834786d
Vivek Raj <iamvrajj@gmail.com>
2019-04-28 22:32:18 +0530

    updated

--- scripts/delete_all_questions.py
+++ scripts/delete_all_questions.py
@@ -10,10 +10,10 @@ def run():
     for i in obj:
         print(i.due_date)
         if i.due_date != now.day:
-            s = smtplib.SMTP('mail.iitp.ac.in', 25)
+            s = smtplib.SMTP(host,port)  #enter mail server host and port number
             s.starttls()
 
-            s.login("vivek.cs16@iitp.ac.in", "eeVaepu2")
+            s.login("emailid", "password") #enter your email id and password.
 
             message = "Your "
             message= message + i.name_of_the_bill
